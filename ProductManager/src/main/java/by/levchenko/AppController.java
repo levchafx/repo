d7158382d272e14975a7ba7593b5440937f36a53
sergeilevchenko <levchafx@gmail.com>
2019-10-21 18:48:26 +0300

    styles added

--- ProductManager/src/main/java/by/levchenko/AppController.java
+++ ProductManager/src/main/java/by/levchenko/AppController.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -31,7 +31,7 @@ public class AppController {
 		return "new_product";
 	}
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@PostMapping(value = "/save")
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		service.save(product);
 		return "redirect:/";
